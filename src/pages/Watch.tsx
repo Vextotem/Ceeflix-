1b80b271aa8ad404e1ddd88d2f67f779b7b574e6
Jayce <kendump666@gmail.com>
2024-10-08 11:01:34 +0800

    Update Watch.tsx

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -51,7 +51,7 @@ export default function Watch() {
   function getTitle() {
     let title = data ? data.title : 'Watch';
 
-    if (type === 'tv') title += ` S${season} E${episode}`;
+    if (type === 'series') title += ` S${season} E${episode}`;
 
     return title;
   }
